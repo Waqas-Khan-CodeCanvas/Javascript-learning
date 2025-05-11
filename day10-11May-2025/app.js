b4cc759480123cb0670b83ee21279be2b2e2e73e
Waqas-Khan-CodeCanvas <waqaskhan11411704@gmail.com>
2025-05-11 14:37:48 +0500

    smit class

---
+++ day10-11May-2025/app.js
@@ -0,0 +1,23 @@
+// // rest and spread operators
+// const fruits  = ["apple","banana"];
+// const newFruitsArray = [...fruits]
+// const vegetables = ["carrot ","lemon"];
+// const foods = [...fruits , ...vegetables]
+
+// foods[0]="water melon";
+// console.log(foods);
+// console.log(fruits);
+// console.log(vegetables);
+
+
+// objects
+const person  = {
+    fullName : "waqas khan",
+    age : 20,
+}
+console.log(person);
+const newPerson = {
+    ...person,
+    fullName: "ali khan",
+}
+console.log(newPerson);
