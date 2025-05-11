cc9b8f4d5fb5350a8b1c0bf01377794462ae388e
Waqas-Khan-CodeCanvas <waqaskhan11411704@gmail.com>
2025-05-11 14:42:42 +0500

    smit class

--- day10-11May-2025/app.js
+++ day10-11May-2025/app.js
@@ -1,23 +1,30 @@
-// // rest and spread operators
-// const fruits  = ["apple","banana"];
-// const newFruitsArray = [...fruits]
-// const vegetables = ["carrot ","lemon"];
-// const foods = [...fruits , ...vegetables]
+// // // rest and spread operators
+// // const fruits  = ["apple","banana"];
+// // const newFruitsArray = [...fruits]
+// // const vegetables = ["carrot ","lemon"];
+// // const foods = [...fruits , ...vegetables]
 
-// foods[0]="water melon";
-// console.log(foods);
-// console.log(fruits);
-// console.log(vegetables);
+// // foods[0]="water melon";
+// // console.log(foods);
+// // console.log(fruits);
+// // console.log(vegetables);
 
 
-// objects
-const person  = {
-    fullName : "waqas khan",
-    age : 20,
-}
-console.log(person);
-const newPerson = {
-    ...person,
-    fullName: "ali khan",
-}
-console.log(newPerson);
+// // objects
+// const person  = {
+//     fullName : "waqas khan",
+//     age : 20,
+// }
+// console.log(person);
+// const newPerson = {
+//     ...person,
+//     fullName: "ali khan",
+// }
+// console.log(newPerson);
+
+const fruits  = ["apple","banana","cherry","peach"];
+const [firstFruit,secondFruit, ...restFruits] = fruits
+
+console.log(firstFruit);
+console.log(secondFruit);
+console.log(restFruits);
